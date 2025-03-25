eb3e2de7fd52d031bea51134d5b5b6203e0cdda6
Mattia Gasparotto <gasmat04@gmail.com>
2025-03-25 21:39:16 +0100

    added basic logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use redis::Client;
 
 use crate::routes::health_check::health_check;
@@ -9,6 +9,7 @@ use crate::routes::edit::edit;
 use crate::routes::remove::remove;
 
 pub fn run(listener: TcpListener, redis: Client) -> Result<Server, std::io::Error> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let redis = web::Data::new(redis);
     let server = HttpServer::new(move || {
         App::new()
@@ -16,6 +17,7 @@ pub fn run(listener: TcpListener, redis: Client) -> Result<Server, std::io::Erro
             .route("/add", web::post().to(add))
             .route("/remove", web::post().to(edit))
             .route("/edit", web::post().to(remove))
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .app_data(redis.clone())
     })
         .listen(listener)?
